a2b625875deadbec93d8727f3e0d7b7e3fdfe9db
Gaurav Salunke <gauravsalunke15@gmail.com>
2023-03-07 17:38:03 +0530

    Implemented changes to get rating and hotel details for user. Feign Clients

--- UserService/src/main/java/com/gaurav/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/gaurav/user/service/services/impl/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.gaurav.user.service.services.impl;
 
+import com.gaurav.user.service.entities.Hotel;
+import com.gaurav.user.service.entities.Rating;
 import com.gaurav.user.service.entities.User;
+import com.gaurav.user.service.exceptions.ResourceNotFoundException;
+import com.gaurav.user.service.external.services.HotelService;
 import com.gaurav.user.service.repositories.UserRepository;
 import com.gaurav.user.service.services.UserService;
 import org.slf4j.Logger;
@@ -8,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,7 +26,11 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @Autowired
+    private HotelService hotelService;
 
     private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
@@ -42,6 +51,31 @@ public class UserServiceImpl implements UserService {
     //get single user
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).get();
+        //get user from database with the help  of user repository
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : " + userId));
+        // fetch rating of the above user from RATING SERVICE
+        //http://localhost:8083/ratings/users/65a2d6b0-0536-4779-abf2-2e0822af52ec
+
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/" + user.getUserId(), Rating[].class);
+        logger.info("{} ", ratingsOfUser);
+        List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            //api call to hotel service to get the hotel
+            //http://localhost:8082/hotels/fbfda7d8-1795-443e-9e30-d96e739e69dc
+
+            // Using Rest Template
+//            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/" + rating.getHotelId(), Hotel.class);
+//            Hotel hotel = forEntity.getBody();
+//            logger.info("response status code: {} ", forEntity.getStatusCode());
+
+            // Using FeignClients
+            Hotel hotel = hotelService.getHotel(rating.getHotelId());
+            rating.setHotel(hotel);
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingList);
+        return user;
     }
+
 }
